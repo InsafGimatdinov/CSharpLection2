548a3ac37ac4f0a4b66ea0c3ccae4da76f9f10ef
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-08 13:18:46 +0300

    Add file about array to continiue

---
+++ Example3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Example3
+{
+    public static void Main()
+    {
+        int[] array = { 1, 23, 4, 55, 64, 5, 6, 564, 5, 5, 6, 456 };
+        int n = array.Length;
+        int find = 5;
+        int index = 0;
+        while (index < n)
+        {
+            if (array[index] == find) 
+            {
+                Console.WriteLine(index);
+                break;
+            }
+            index++;
+        }
+    }
+}
